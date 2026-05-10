2a95a20d60e373ddbf781af311abcdc62a767129
rladbals4951 <youmin2271072@ewha.ac.kr>
2026-05-10 23:47:57 +0900

    Upload: HW18_AR_Test

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVZUU0NDVC8vLy8vQUFBQm1XL2R3eEYrV2swcXNDK2ZTWnJGT3FwUUNiR1dKUk03bGJQdzNtd1M2WWVQSEVZbDV6M2U1MUkvQ1VMaVZHUUlTb1pSZ3IrVWgvRkVBSTBkMGprN3ZkbVRQK0ZaVmY0ajBJM2dONm91eUlEOGhwSHJkWjdBVGErRkx6b2pvb1EyVzU2Y3lZQ08rUTFiN0VHb3ZxanpWSEVQWFU3UkRneEtVcGYwQm1vdy9tZ1M0N2Nkb2RoOFRxN2NZZ1FCWVlyc0dxWU1tanFwc09DaTdsdlJ5QWg4d2x1VTRNT1FSQVNFaXpUNm1ISWx5dnBHSm9LN0NrMEVnUWZlREI5WHlweFVtdUMwSmpDRkQzNkZCQTlsWXRlalR4bGRmSVRsTFFHempZSGxGYmFkQlFlQ1Vla3Y1dlFnalVCRWhUMGhEc0dsc0lDSUlzTC9WcDRNekduUHl3SHFTU2l3WHlJSjBSTTZ4dnladkZoTHUyZVY=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
